5c9646e3defe2a48015e4fce31a413b71ff35503
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-22 03:43:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,15 @@
+<<<<<<< HEAD
 def unsafe?(speed)
 if speed < 40 || speed > 60
 	return	true
 	else
 		return false
+=======
+if speed < 40 || speed > 60
+		true
+	else
+		false
+>>>>>>> 8c7d65d726369ef0fce737035eb5178c108c3595
 	end
 end
 
